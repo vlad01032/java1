969240dc6ac48f5006fc82dbbf774f87b959818a
vlad <vlad01032@yandex.ru>
2020-08-07 21:26:33 +0300

    new

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.bitsworld;
 public class SumBits {
     public static int sumBits(byte value){
         int result = 0;
-        for (int i = 0; i < 7; i++){
+        for (int i = 0; i < 8; i++){
             if ((value & 1) == 1) result += 1;
             value >>= 1;
         }
