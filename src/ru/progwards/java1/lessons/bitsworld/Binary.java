3770af3b93888898687d837e8e5cec982eb42dec
vlad <vlad01032@yandex.ru>
2020-08-09 10:48:49 +0300

    new

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -1,24 +1,21 @@
 package ru.progwards.java1.lessons.bitsworld;
 
+import java.util.Objects;
+
 public class Binary {
     byte num;
     public Binary(byte num){
        this.num = num;
+
     }
 
     public String toString(){
         String result = "";
-        String val = "0";
-        boolean flag = num > 0;
+        String val = num < 0 ? "1" : "0";
 
         if (num == 0) return "00000000";
 
-        if (!flag) {
-            num *= -1;
-            val = "1";
-        }
-
-        while (num > 0) {
+        while (num < -1 || num > 0 ) {
             if ((num & 1) == 0){
                 result = "0" + result;
             } else {
@@ -27,15 +24,17 @@ public class Binary {
             num >>= 1;
         }
 
-        while (result.length() < 8){
-            result = val + result;
+        while (result.length() < 7){
+            result = "0" + result;
         }
+
+        result = val + result;
 
         return result;
     }
 
     public static void main(String[] args) {
-        Binary ss = new Binary((byte) 3);
+        Binary ss = new Binary((byte) 128);
         System.out.println(ss.toString());
     }
 }
