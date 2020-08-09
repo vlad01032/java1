af2a1cbb5f60ebe89e3e37d6ab52c66094158d0a
vlad <vlad01032@yandex.ru>
2020-08-09 15:24:59 +0300

    new

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -3,52 +3,42 @@ package ru.progwards.java1.lessons.bitsworld;
 import java.util.Objects;
 
 public class Binary {
-    byte num;
+    int num;
     public Binary(byte num){
-       this.num = num;
-
+       this.num = (int) num;
     }
 
     public String toString(){
-        String result = "";
-        String val = num < 0 ? "1" : "0";
-
         if (num == 0) return "00000000";
 
-        if (num < 0){
-            while (num < -1) {
-                if ((num & 1) == 0){
-                    result = "0" + result;
-                } else {
-                    result = "1" + result;
-                }
-                num >>= 1;
+        String result = "";
+        String sign = num < 0 ? "1" : "0";
+
+        if (num < 0) num *= -1;
+
+        while (num > 1){
+            if ((num & 1) == 0){
+                result = "0" + result;
+            } else {
+                result = "1" + result;
             }
-            result = "1" + result;
-        } else {
-            while (num > 0 ) {
-                if ((num & 1) == 0){
-                    result = "0" + result;
-                } else {
-                    result = "1" + result;
-                }
-                num >>= 1;
+            num >>= 1;
+        }
+        result = num + result;
+
+        if (result.length() <= 7) {
+
+            while (result.length() < 7) {
+                result = "0" + result;
             }
+
+            result = sign + result;
         }
-
-
-
-        while (result.length() < 7){
-            result = "0" + result;
-        }
-
-        if (result.length() < 8) result = val + result;
-
         return result;
     }
 
     public static void main(String[] args) {
-        Binary ss = new Binary((byte) 128);
+        Binary ss = new Binary((byte) 10);
         System.out.println(ss.toString());
     }
 }
